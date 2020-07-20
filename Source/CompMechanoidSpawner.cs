3840e0c8c3bf218982c09c16cfc6780ddc2aeca9
Ogliss <randomdragon2@hotmail.com>
2020-07-20 20:45:49 +0100

    tweaks

---
+++ Source/CompMechanoidSpawner.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Mechanionjoin
+{
+
+	public class CompProperties_MechanoidSpawner : CompProperties
+	{
+		public CompProperties_MechanoidSpawner()
+		{
+			this.compClass = typeof(CompMechanoidSpawner);
+
+		}
+		public CompProperties_MechanoidSpawner(PawnKindDef PawnKind)
+		{
+			this.compClass = typeof(CompMechanoidSpawner);
+			this.PawnKind = PawnKind;
+		}
+		public PawnKindDef PawnKind;
+	}
+	// Token: 0x020000CC RID: 204
+	public class CompMechanoidSpawner : ThingComp
+	{
+		public CompProperties_MechanoidSpawner Props => (CompProperties_MechanoidSpawner)this.props;
+		// Token: 0x06000254 RID: 596 RVA: 0x00018554 File Offset: 0x00016754
+		public override void CompTick()
+		{
+			this.CheckShouldSpawn();
+		}
+
+		// Token: 0x06000255 RID: 597 RVA: 0x00018560 File Offset: 0x00016760
+		private void CheckShouldSpawn()
+		{
+			this.SpawnMe();
+			this.parent.Destroy(0);
+		}
+
+		// Token: 0x06000256 RID: 598 RVA: 0x00018588 File Offset: 0x00016788
+		public void SpawnMe()
+		{
+
+			PawnGenerationRequest req = new PawnGenerationRequest(Props.PawnKind, Faction.OfPlayer);
+			Pawn pawn  = PawnGenerator.GeneratePawn(req);
+			GenSpawn.Spawn(pawn, this.parent.Position, this.parent.Map, 0);
+		}
+
+	}
+
+}
+	
+
+
+
+
+
---
+++ Source/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using System.Threading.Tasks;
+using Verse;
+using System.Reflection;
+using RimWorld;
+
+namespace Mechanionjoin
+{ 
+    [StaticConstructorOnStartup]
+    static class Main
+    {
+        static Main() 
+        {
+
+			foreach (PawnKindDef kindDef in DefDatabase<PawnKindDef>.AllDefs.ToList<PawnKindDef>())
+			{
+				if (kindDef.RaceProps.Humanlike)
+				{
+					continue;
+				}
+				if (!kindDef.RaceProps.IsMechanoid)
+				{
+					continue;
+				}
+				Log.Message("Mechanoid ");
+				ThingDef thingDef2 = new ThingDef();
+				thingDef2.category = ThingCategory.Item;
+				thingDef2.thingClass = typeof(ThingWithComps);
+				thingDef2.selectable = true;
+				thingDef2.tickerType = TickerType.Normal;
+				thingDef2.altitudeLayer = AltitudeLayer.ItemImportant;
+				thingDef2.scatterableOnMapGen = false;
+				thingDef2.SetStatBaseValue(StatDefOf.Beauty, -4f);
+				thingDef2.alwaysHaulable = true;
+				thingDef2.soundDrop = SoundDefOf.Standard_Drop;
+				thingDef2.pathCost = 15;
+				thingDef2.socialPropernessMatters = false;
+				thingDef2.tradeability = Tradeability.None;
+				thingDef2.messageOnDeteriorateInStorage = false;
+				thingDef2.comps.Add(new CompProperties_MechanoidSpawner(kindDef));
+				thingDef2.defName = "MechanoidEngram_" + kindDef.defName;
+				thingDef2.label = "MechanoidEngram_Label".Translate(kindDef.label);
+				thingDef2.description = "MechanoidEngram_Desc".Translate(kindDef.label);
+				thingDef2.SetStatBaseValue(StatDefOf.MarketValue, 2000);
+				thingDef2.SetStatBaseValue(StatDefOf.Flammability, 0);
+				thingDef2.SetStatBaseValue(StatDefOf.MaxHitPoints, 100);
+				thingDef2.SetStatBaseValue(StatDefOf.WorkToMake, 10 * kindDef.combatPower);
+				thingDef2.SetStatBaseValue(StatDefOf.Mass, 1);
+				thingDef2.thingCategories = new List<ThingCategoryDef>();
+				thingDef2.thingCategories.Add(ThingCategoryDefOf.Manufactured);
+				thingDef2.tradeTags = new List<string>();
+				thingDef2.tradeTags.Add("ExoticMisc");
+				thingDef2.thingSetMakerTags = new List<string>(); ;
+				thingDef2.thingSetMakerTags.Add("RewardStandardLowFreq");
+				thingDef2.thingSetMakerTags.Add("RewardStandardQualitySuper");
+				thingDef2.modContentPack = kindDef.modContentPack;
+				Log.Message(" generated");
+				DefDatabase<ThingDef>.Add(thingDef2);
+			}
+        }
+    }
+}
